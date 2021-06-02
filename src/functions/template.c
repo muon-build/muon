8a4219886e73227330b948a7c7df6b7424759fef
Stone Tickle <lattis@mochiro.moe>
2021-06-02 13:29:56 +0900

    add template files for src/functions/{h,c}

---
+++ src/functions/template.c
@@ -0,0 +1,16 @@
+#include "posix.h"
+
+#include "functions/common.h"
+#include "functions/xxx.h"
+#include "interpreter.h"
+#include "log.h"
+
+static bool
+func_(struct workspace *wk, uint32_t rcvr, uint32_t args_node, uint32_t *obj)
+{
+}
+
+const struct func_impl_name impl_tbl_xxx[] = {
+	{ "", func_ },
+	{ NULL, NULL },
+};
