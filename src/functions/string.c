e281af009975230ac5edf98d34af68f89aef4d0c
Stone Tickle <lattis@mochiro.moe>
2021-06-02 09:03:54 +0900

    implement string.to_upper()

--- src/functions/string.c
+++ src/functions/string.c
@@ -30,7 +30,28 @@ func_strip(struct workspace *wk, uint32_t rcvr, uint32_t args_node, uint32_t *ob
 	return true;
 }
 
+static bool
+func_to_upper(struct workspace *wk, uint32_t rcvr, uint32_t args_node, uint32_t *obj)
+{
+	if (!interp_args(wk, args_node, NULL, NULL, NULL)) {
+		return false;
+	}
+
+	make_obj(wk, obj, obj_string)->dat.str = wk_str_push(wk, wk_objstr(wk, rcvr));
+
+	char *s = wk_objstr(wk, *obj);
+
+	for (; *s; ++s) {
+		if ('a' <= *s && *s <= 'z') {
+			*s -= 32;
+		}
+	}
+
+	return true;
+}
+
 const struct func_impl_name impl_tbl_string[] = {
 	{ "strip", func_strip },
+	{ "to_upper", func_to_upper },
 	{ NULL, NULL },
 };
