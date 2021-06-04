0f182a790a5cedeaa8f1697b10b2dbda4d68e75d
Stone Tickle <lattis@mochiro.moe>
2021-06-04 12:26:33 +0900

    implement external_program.path()

--- src/functions/external_program.c
+++ src/functions/external_program.c
@@ -18,7 +18,21 @@ func_external_program_found(struct workspace *wk, uint32_t rcvr, uint32_t args_n
 	return true;
 }
 
+static bool
+func_external_program_path(struct workspace *wk, uint32_t rcvr, uint32_t args_node, uint32_t *obj)
+{
+	if (!interp_args(wk, args_node, NULL, NULL, NULL)) {
+		return false;
+	}
+
+	struct obj *res = make_obj(wk, obj, obj_string);
+	res->dat.str = get_obj(wk, rcvr)->dat.external_program.full_path;
+
+	return true;
+}
+
 const struct func_impl_name impl_tbl_external_program[] = {
 	{ "found", func_external_program_found },
+	{ "path", func_external_program_path },
 	{ NULL, NULL },
 };
