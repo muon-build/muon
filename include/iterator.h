b9e4986909dbd1fe809c654d191214fe2e1f53d6
Stone Tickle <lattis@mochiro.moe>
2021-07-13 15:37:38 +0900

    change values of enum iteration_result
    Change
    ir_err -> 0
    ir_cont -> 1
    ir_result -> 2
    
    This means that now if you accidentally return false when you meant to
    return ir_err, the effect will be the same.  Previously, it would have
    interpreted false as ir_cont, and kept on going.

--- include/iterator.h
+++ include/iterator.h
@@ -1,9 +1,9 @@
 #ifndef SHARED_TYPES_ITERATOR_H
 #define SHARED_TYPES_ITERATOR_H
 enum iteration_result {
+	ir_err,
 	ir_cont,
 	ir_done,
-	ir_err,
 };
 
 typedef enum iteration_result (*iterator_func)(void *ctx, void *val);
