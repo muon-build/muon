14581c92f38c68cea9c5839534903a65c93b3123
Stone Tickle <lattis@mochiro.moe>
2021-07-19 08:17:45 +0900

    add CFLAGS and LDFLAGS to bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -63,6 +63,6 @@ cat \
 	src/version.c.in \
 	src/workspace.c \
 	src/wrap.c \
-	| cc -g -Iinclude $(pkgconf --cflags libpkgconf) -x c -o "$dir/muon.o" -c -
+	| cc ${CFLAGS:-} -g -Iinclude $(pkgconf --cflags libpkgconf) -x c -o "$dir/muon.o" -c -
 
-cc "$dir/muon.o" $(pkgconf --libs libpkgconf) -o "$dir/muon"
+cc ${LDFLAGS:-} $(pkgconf --libs libpkgconf) "$dir/muon.o" -o "$dir/muon"
