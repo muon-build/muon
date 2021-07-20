69ae4bc302a87e133af2fccf81392a4c5bc61308
Stone Tickle <lattis@mochiro.moe>
2021-07-20 15:27:52 +0900

    use error_unrecoverable instead of assert for oom

--- src/mem.c
+++ src/mem.c
@@ -1,34 +1,51 @@
 #include "posix.h"
 
-#include <stdlib.h>
 #include <assert.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
 
+#include "eval.h"
 #include "mem.h"
 
 void *
 z_calloc(size_t nmemb, size_t size)
 {
+	assert(size);
 	void *ret;
 	ret = calloc(nmemb, size);
-	assert(ret && "calloc failed");
+
+	if (!ret) {
+		error_unrecoverable("calloc failed: %s", strerror(errno));
+	}
 	return ret;
 }
 
 void *
 z_malloc(size_t size)
 {
+	assert(size);
 	void *ret;
 	ret = malloc(size);
-	assert(ret && "malloc failed");
+
+	if (!ret) {
+		error_unrecoverable("malloc failed: %s", strerror(errno));
+	}
+
 	return ret;
 }
 
 void *
 z_realloc(void *ptr, size_t size)
 {
+	assert(size);
 	void *ret;
 	ret = realloc(ptr, size);
-	assert(ret && "realloc failed");
+
+	if (!ret) {
+		error_unrecoverable("realloc failed: %s", strerror(errno));
+	}
+
 	return ret;
 }
 
