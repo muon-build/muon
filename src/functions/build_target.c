a802c1fcf5fdfddc49baea9d0de4af5a990aaa4e
Stone Tickle <lattis@mochiro.moe>
2021-08-12 08:12:42 -0500

    implement build_target.full_path()

--- src/functions/build_target.c
+++ src/functions/build_target.c
@@ -4,6 +4,7 @@
 #include "functions/common.h"
 #include "lang/interpreter.h"
 #include "log.h"
+#include "platform/path.h"
 
 static bool
 func_build_target_name(struct workspace *wk, uint32_t rcvr, uint32_t args_node, uint32_t *obj)
@@ -16,7 +17,26 @@ func_build_target_name(struct workspace *wk, uint32_t rcvr, uint32_t args_node, 
 	return true;
 }
 
+static bool
+func_build_target_full_path(struct workspace *wk, uint32_t rcvr, uint32_t args_node, uint32_t *obj)
+{
+	if (!interp_args(wk, args_node, NULL, NULL, NULL)) {
+		return false;
+	}
+
+	struct obj *tgt = get_obj(wk, rcvr);
+
+	char path[PATH_MAX];
+	if (!path_join(path, PATH_MAX, wk_str(wk, tgt->dat.tgt.build_dir), wk_str(wk, tgt->dat.tgt.build_name))) {
+		return false;
+	}
+
+	*obj = make_str(wk, path);
+	return true;
+}
+
 const struct func_impl_name impl_tbl_build_target[] = {
 	{ "name", func_build_target_name },
+	{ "full_path", func_build_target_full_path },
 	{ NULL, NULL },
 };
