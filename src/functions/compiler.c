d31b44010492ac3d8184716e680ce6da72270c0e
Stone Tickle <lattis@mochiro.moe>
2021-08-24 18:46:16 -0500

    implement compiler.has_argument()

--- src/functions/compiler.c
+++ src/functions/compiler.c
@@ -3,6 +3,7 @@
 #include <limits.h>
 #include <string.h>
 
+#include "args.h"
 #include "buf_size.h"
 #include "coerce.h"
 #include "compilers.h"
@@ -18,17 +19,64 @@
 
 struct func_compiler_get_supported_arguments_iter_ctx {
 	uint32_t arr, node, compiler;
-	char *test_c, *test_o;
 };
 
-static enum iteration_result
-func_compiler_get_supported_arguments_iter(struct workspace *wk, void *_ctx, uint32_t val_id)
+static bool
+output_test_source(struct workspace *wk, enum compiler_language l, const char **source_file, const char **out_file)
 {
-	struct func_compiler_get_supported_arguments_iter_ctx *ctx = _ctx;
+	static bool already_output[compiler_language_count];
+	static char test_source_path[PATH_MAX], test_out_path[PATH_MAX];
+	const char *test_name = NULL, *test_source = NULL;
 
-	struct obj *comp = get_obj(wk, ctx->compiler);
+	switch (l) {
+	case compiler_language_c_hdr:
+	case compiler_language_cpp_hdr:
+	case compiler_language_count:
+		LOG_E("no test source for language: %s", compiler_language_to_s(l));
+		return false;
+	case compiler_language_c:
+		test_name = "test.c";
+		test_source = "int main(void){}\n";
+		break;
+	case compiler_language_cpp:
+		test_name = "test.cpp";
+		test_source = "int main(void){}\n";
+		break;
+	}
 
-	if (!typecheck(wk, ctx->node, val_id, obj_string)) {
+	assert(test_name && test_source);
+
+	strcpy(test_out_path, test_source_path);
+	if (!path_add_suffix(test_out_path, PATH_MAX, ".o")) {
+		return false;
+	}
+
+	*source_file = test_source_path;
+	*out_file = test_out_path;
+
+	if (already_output[l]) {
+		return true;
+	}
+	already_output[l] = true;
+
+	if (!output_private_file(wk, test_source_path, test_name, test_source)) {
+		return false;
+	}
+
+	return true;
+}
+
+static bool
+compiler_has_argument(struct workspace *wk, uint32_t comp_id, uint32_t err_node, uint32_t arg, bool *has_argument)
+{
+	struct obj *comp = get_obj(wk, comp_id);
+
+	const char *test_source, *test_out;
+	if (!output_test_source(wk, comp->dat.compiler.lang, &test_source, &test_out)) {
+		return ir_err;
+	}
+
+	if (!typecheck(wk, err_node, arg, obj_string)) {
 		return ir_err;
 	}
 
@@ -38,32 +86,64 @@ func_compiler_get_supported_arguments_iter(struct workspace *wk, void *_ctx, uin
 	const char *argv[MAX_ARGS + 1] = { name };
 	uint32_t len = 1;
 	push_argv(argv, &len, MAX_ARGS, compilers[t].args.werror());
-	push_argv_single(argv, &len, MAX_ARGS, wk_objstr(wk, val_id));
+	push_argv_single(argv, &len, MAX_ARGS, wk_objstr(wk, arg));
 	push_argv(argv, &len, MAX_ARGS, compilers[t].args.compile_only());
-	push_argv(argv, &len, MAX_ARGS, compilers[t].args.output(ctx->test_o));
-	push_argv_single(argv, &len, MAX_ARGS, ctx->test_c);
+	push_argv(argv, &len, MAX_ARGS, compilers[t].args.output(test_out));
+	push_argv_single(argv, &len, MAX_ARGS, test_source);
 
 	struct run_cmd_ctx cmd_ctx = { 0 };
 	if (!run_cmd(&cmd_ctx, name, (char **)argv)) {
 		if (cmd_ctx.err_msg) {
-			interp_error(wk, ctx->node, "error: %s", cmd_ctx.err_msg);
+			interp_error(wk, err_node, "error: %s", cmd_ctx.err_msg);
 		} else {
-			interp_error(wk, ctx->node, "error: %s", strerror(cmd_ctx.err_no));
+			interp_error(wk, err_node, "error: %s", strerror(cmd_ctx.err_no));
 		}
 		return ir_err;
 	}
 
+	*has_argument = cmd_ctx.status == 0;
+
 	LOG_I("'%s' supported: %s",
-		wk_objstr(wk, val_id),
-		cmd_ctx.status == 0 ? "\033[32mYES\033[0m" : "\033[31mNO\033[0m"
+		wk_objstr(wk, arg),
+		*has_argument ? "\033[32mYES\033[0m" : "\033[31mNO\033[0m"
 		);
 
-	if (cmd_ctx.status == 0) {
+	return true;
+}
+
+static enum iteration_result
+func_compiler_get_supported_arguments_iter(struct workspace *wk, void *_ctx, uint32_t val_id)
+{
+	struct func_compiler_get_supported_arguments_iter_ctx *ctx = _ctx;
+	bool has_argument;
+
+	if (!compiler_has_argument(wk, ctx->compiler, ctx->node, val_id, &has_argument)) {
+		return false;
+	}
+
+	if (has_argument) {
 		obj_array_push(wk, ctx->arr, val_id);
 	}
 
-
 	return ir_cont;
+}
+
+static bool
+func_compiler_has_argument(struct workspace *wk, uint32_t rcvr, uint32_t args_node, uint32_t *obj)
+{
+	struct args_norm an[] = { { obj_string }, ARG_TYPE_NULL };
+
+	if (!interp_args(wk, args_node, an, NULL, NULL)) {
+		return false;
+	}
+
+	bool has_argument;
+	if (!compiler_has_argument(wk, rcvr, an[0].node, an[0].val, &has_argument)) {
+		return false;
+	}
+
+	make_obj(wk, obj, obj_bool)->dat.boolean = has_argument;
+	return true;
 }
 
 static bool
@@ -77,19 +157,9 @@ func_compiler_get_supported_arguments(struct workspace *wk, uint32_t rcvr, uint3
 
 	make_obj(wk, obj, obj_array);
 
-	char test_c[PATH_MAX], test_o[PATH_MAX];
-	output_private_file(wk, test_c, "test.c", "int main(void){}\n");
-
-	strcpy(test_o, test_c);
-	if (!path_add_suffix(test_o, PATH_MAX, ".o")) {
-		return false;
-	}
-
 	return obj_array_foreach(wk, an[0].val,
 		&(struct func_compiler_get_supported_arguments_iter_ctx) {
 		.compiler = rcvr,
-		.test_c = test_c,
-		.test_o = test_o,
 		.arr = *obj,
 		.node = an[0].node,
 	}, func_compiler_get_supported_arguments_iter);
@@ -170,6 +240,7 @@ done:
 
 const struct func_impl_name impl_tbl_compiler[] = {
 	{ "get_supported_arguments", func_compiler_get_supported_arguments },
+	{ "has_argument", func_compiler_has_argument },
 	{ "get_id", func_compiler_get_id },
 	{ "find_library", func_compiler_find_library },
 	{ NULL, NULL },
