69902a019cebe1dd98b494f3c05e6def274b9edf
Stone Tickle <lattis@mochiro.moe>
2021-08-24 19:05:25 -0500

    add a bogus endian function

--- src/functions/machine.c
+++ src/functions/machine.c
@@ -17,7 +17,20 @@ func_machine_system(struct workspace *wk, uint32_t rcvr, uint32_t args_node, uin
 	return true;
 }
 
+static bool
+func_machine_endian(struct workspace *wk, uint32_t rcvr, uint32_t args_node, uint32_t *obj)
+{
+	if (!interp_args(wk, args_node, NULL, NULL, NULL)) {
+		return false;
+	}
+
+	make_obj(wk, obj, obj_string)->dat.str = wk_str_push(wk, "little");
+
+	return true;
+}
+
 const struct func_impl_name impl_tbl_machine[] = {
 	{ "system", func_machine_system },
+	{ "endian", func_machine_endian },
 	{ NULL, NULL },
 };
