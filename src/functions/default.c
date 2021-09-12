58020782cb4ed5f4c8028ff7ead687bdad284ee7
Eli Schwartz <eschwartz@archlinux.org>
2021-09-12 19:47:16 -0400

    include_directories: accept multiple args
    e.g. include_directories('incdir', 'incdir2') is valid and should
    produce two -I paths.

--- src/functions/default.c
+++ src/functions/default.c
@@ -308,7 +308,7 @@ func_find_program(struct workspace *wk, uint32_t _, uint32_t args_node, uint32_t
 static bool
 func_include_directories(struct workspace *wk, uint32_t _, uint32_t args_node, uint32_t *obj)
 {
-	struct args_norm an[] = { { obj_any }, ARG_TYPE_NULL };
+	struct args_norm an[] = { { ARG_TYPE_GLOB }, ARG_TYPE_NULL };
 
 	if (!interp_args(wk, args_node, an, NULL, NULL)) {
 		return false;
