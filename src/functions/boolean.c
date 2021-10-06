2ab273ce725954aa54fbcc84988b760343d8c94b
Stone Tickle <lattis@mochiro.moe>
2021-10-05 20:03:52 -0500

    add boolean.to_string

--- src/functions/boolean.c
+++ src/functions/boolean.c
@@ -6,25 +6,33 @@
 #include "log.h"
 
 static bool
-func_boolean_to_int(struct workspace *wk, uint32_t rcvr, uint32_t args_node, uint32_t *obj)
+func_boolean_to_string(struct workspace *wk, obj rcvr, uint32_t args_node, obj *res)
 {
 	if (!interp_args(wk, args_node, NULL, NULL, NULL)) {
 		return false;
 	}
 
-	int32_t val;
-	if (get_obj(wk, rcvr)->dat.boolean) {
-		val = 1;
-	} else {
-		val = 0;
+	const char *s = get_obj(wk, rcvr)->dat.boolean ? "true" : "false";
+	*res = make_str(wk, s);
+
+	return true;
+}
+
+static bool
+func_boolean_to_int(struct workspace *wk, obj rcvr, uint32_t args_node, obj *res)
+{
+	if (!interp_args(wk, args_node, NULL, NULL, NULL)) {
+		return false;
 	}
 
-	make_obj(wk, obj, obj_number)->dat.num = val;
+	int32_t val = get_obj(wk, rcvr)->dat.boolean ? 1 : 0;
+	make_obj(wk, res, obj_number)->dat.num = val;
 
 	return true;
 }
 
 const struct func_impl_name impl_tbl_boolean[] = {
 	{ "to_int", func_boolean_to_int },
+	{ "to_string", func_boolean_to_string },
 	{ NULL, NULL },
 };
