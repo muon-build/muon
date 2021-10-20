21eecf0bcf7095f5b9b4f47abf3bd5a479dd1e81
Stone Tickle <lattis@mochiro.moe>
2021-10-20 09:52:23 -0500

    add number.is_even/is_odd

--- src/functions/number.c
+++ src/functions/number.c
@@ -6,13 +6,35 @@
 #include "log.h"
 
 static bool
-func_number_to_string(struct workspace *wk, uint32_t rcvr, uint32_t args_node, uint32_t *obj)
+func_number_is_odd(struct workspace *wk, obj rcvr, uint32_t args_node, obj *res)
 {
 	if (!interp_args(wk, args_node, NULL, NULL, NULL)) {
 		return false;
 	}
 
-	struct obj *str = make_obj(wk, obj, obj_string);
+	make_obj(wk, res, obj_bool)->dat.boolean = (get_obj(wk, rcvr)->dat.num & 1) != 0;
+	return true;
+}
+
+static bool
+func_number_is_even(struct workspace *wk, obj rcvr, uint32_t args_node, obj *res)
+{
+	if (!interp_args(wk, args_node, NULL, NULL, NULL)) {
+		return false;
+	}
+
+	make_obj(wk, res, obj_bool)->dat.boolean = (get_obj(wk, rcvr)->dat.num & 1) == 0;
+	return true;
+}
+
+static bool
+func_number_to_string(struct workspace *wk, obj rcvr, uint32_t args_node, obj *res)
+{
+	if (!interp_args(wk, args_node, NULL, NULL, NULL)) {
+		return false;
+	}
+
+	struct obj *str = make_obj(wk, res, obj_string);
 	str->dat.str = wk_str_pushf(wk, "%ld", (intmax_t)get_obj(wk, rcvr)->dat.num);
 
 	return true;
@@ -20,5 +42,7 @@ func_number_to_string(struct workspace *wk, uint32_t rcvr, uint32_t args_node, u
 
 const struct func_impl_name impl_tbl_number[] = {
 	{ "to_string", func_number_to_string },
+	{ "is_even", func_number_is_even },
+	{ "is_odd", func_number_is_odd },
 	{ NULL, NULL },
 };
