5b306da67e95b7771e26f9552a1fe962ad8e99eb
Stone Tickle <lattis@mochiro.moe>
2021-11-03 07:36:22 -0500

    impl setup reading dumped options from file
    - fixes build regeneration

--- src/functions/default/setup.c
+++ src/functions/default/setup.c
@@ -5,6 +5,7 @@
 #include "functions/default/setup.h"
 #include "lang/eval.h"
 #include "lang/interpreter.h"
+#include "lang/serial.h"
 #include "log.h"
 #include "platform/filesystem.h"
 #include "platform/path.h"
@@ -38,12 +39,12 @@ struct set_options_ctx {
 };
 
 static enum iteration_result
-set_options_iter(struct workspace *wk, void *_ctx, uint32_t key, uint32_t val)
+set_options_iter(struct workspace *wk, void *_ctx, obj key, obj v)
 {
 	struct set_options_ctx *ctx = _ctx;
+	struct obj *val = get_obj(wk, v);
 
-	switch (get_obj(wk, val)->type) {
-	case obj_dict:
+	if (val->type == obj_dict) {
 		if (ctx->parent) {
 			interp_error(wk, ctx->err_node, "options nested too deep");
 			return ir_err;
@@ -51,34 +52,59 @@ set_options_iter(struct workspace *wk, void *_ctx, uint32_t key, uint32_t val)
 
 		ctx->parent = key;
 
-		if (!obj_dict_foreach(wk, val, ctx, set_options_iter)) {
+		if (!obj_dict_foreach(wk, v, ctx, set_options_iter)) {
 			return ir_err;
 		}
 
 		ctx->parent = 0;
-		break;
-	default: {
-		struct option_override oo = { .obj_value = true };
-		if (ctx->parent) {
-			oo.proj = wk_str_push(ctx->sub_wk, get_cstr(wk, ctx->parent));
-		}
-
-		oo.name = str_clone(wk, ctx->sub_wk, key);
-
-		if (!obj_clone(wk, ctx->sub_wk, val, &oo.val)) {
-			return ir_err;
-		}
-
-		darr_push(&ctx->sub_wk->option_overrides, &oo);
-		break;
+		return ir_cont;
 	}
+
+	obj opt_val;
+
+	if (val->type == obj_option) {
+		opt_val = val->dat.option.val;
+	} else {
+		opt_val = v;
 	}
+
+	struct option_override oo = { .obj_value = true };
+	if (ctx->parent) {
+		oo.proj = wk_str_push(ctx->sub_wk, get_cstr(wk, ctx->parent));
+	}
+
+	oo.name = str_clone(wk, ctx->sub_wk, key);
+
+	if (!obj_clone(wk, ctx->sub_wk, opt_val, &oo.val)) {
+		return ir_err;
+	}
+
+	darr_push(&ctx->sub_wk->option_overrides, &oo);
 
 	return ir_cont;
 }
 
+static bool
+set_options_from_file(struct workspace *wk, struct set_options_ctx *ctx, obj file)
+{
+	struct obj *fname = get_obj(wk, file);
+	assert(fname->type == obj_file);
+
+	FILE *f;
+	if (!(f = fs_fopen(get_cstr(wk, fname->dat.file), "r"))) {
+		return false;
+	}
+
+	obj opts;
+	if (!serial_load(wk, &opts, f)) {
+		return false;
+	}
+
+	return obj_dict_foreach(wk, opts, ctx, set_options_iter);
+}
+
 bool
-func_setup(struct workspace *wk, uint32_t _, uint32_t args_node, uint32_t *obj)
+func_setup(struct workspace *wk, uint32_t _, uint32_t args_node, obj *res)
 {
 	struct args_norm an[] = { { obj_string }, ARG_TYPE_NULL };
 	enum kwargs {
@@ -88,7 +114,7 @@ func_setup(struct workspace *wk, uint32_t _, uint32_t args_node, uint32_t *obj)
 	};
 	struct args_kw akw[] = {
 		[kw_source] = { "source", obj_string },
-		[kw_options] = { "options", obj_dict },
+		[kw_options] = { "options", obj_any },
 		[kw_cross] = { "cross", obj_string },
 		0
 	};
@@ -100,7 +126,6 @@ func_setup(struct workspace *wk, uint32_t _, uint32_t args_node, uint32_t *obj)
 	struct workspace sub_wk;
 	bool ret = false;
 	char build_ninja[PATH_MAX];
-	uint32_t project_id;
 
 	if (akw[kw_source].set) {
 		L("chdir to '%s'", get_cstr(wk, akw[kw_source].val));
@@ -116,12 +141,26 @@ func_setup(struct workspace *wk, uint32_t _, uint32_t args_node, uint32_t *obj)
 	}
 
 	if (akw[kw_options].set) {
+		obj val;
+		if (!obj_array_flatten_one(wk, akw[kw_options].val, &val)) {
+			interp_error(wk, akw[kw_options].node, "could not flatten argument");
+		}
+
+		struct obj *opts = get_obj(wk, val);
 		struct set_options_ctx ctx = {
 			.sub_wk = &sub_wk,
 			.err_node = akw[kw_options].node
 		};
 
-		if (!obj_dict_foreach(wk, akw[kw_options].val, &ctx, set_options_iter)) {
+		if (opts->type == obj_dict) {
+			if (!obj_dict_foreach(wk, val, &ctx, set_options_iter)) {
+				goto ret;
+			}
+		} else if (opts->type == obj_file) {
+			set_options_from_file(wk, &ctx, val);
+		} else {
+			interp_error(wk, akw[kw_options].node, "expected dict or file for options, got %s",
+				obj_type_to_s(opts->type));
 			goto ret;
 		}
 	}
