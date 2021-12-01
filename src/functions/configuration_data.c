fc073f97c32e2ccaca15a54b0350ccb929f0a30e
Stone Tickle <lattis@mochiro.moe>
2021-11-30 19:00:24 -0600

    add configuration_data.has()

--- src/functions/configuration_data.c
+++ src/functions/configuration_data.c
@@ -193,12 +193,27 @@ func_configuration_data_keys(struct workspace *wk, obj rcvr, uint32_t args_node,
 	return true;
 }
 
+static bool
+func_configuration_data_has(struct workspace *wk, obj rcvr, uint32_t args_node, obj *res)
+{
+	struct args_norm an[] = { { obj_string }, ARG_TYPE_NULL };
+
+	if (!interp_args(wk, args_node, an, NULL, NULL)) {
+		return false;
+	}
+
+	obj _, dict = get_obj(wk, rcvr)->dat.configuration_data.dict;
+	make_obj(wk, res, obj_bool)->dat.boolean = obj_dict_index(wk, dict, an[0].val, &_);
+	return true;
+}
+
 const struct func_impl_name impl_tbl_configuration_data[] = {
+	{ "get", func_configuration_data_get },
+	{ "get_unquoted", func_configuration_data_get_unquoted },
+	{ "has", func_configuration_data_has },
+	{ "keys", func_configuration_data_keys },
 	{ "set", func_configuration_data_set },
 	{ "set10", func_configuration_data_set10 },
 	{ "set_quoted", func_configuration_data_set_quoted },
-	{ "get", func_configuration_data_get },
-	{ "get_unquoted", func_configuration_data_get_unquoted },
-	{ "keys", func_configuration_data_keys },
 	{ NULL, NULL },
 };
