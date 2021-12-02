730d0d133da95c78031f2afcb663b3d8644b9b7b
Stone Tickle <lattis@mochiro.moe>
2021-12-01 20:16:35 -0600

    implement custom_target.full_path()

--- src/functions/custom_target.c
+++ src/functions/custom_target.c
@@ -6,17 +6,36 @@
 #include "log.h"
 
 static bool
-func_to_list(struct workspace *wk, uint32_t rcvr, uint32_t args_node, uint32_t *obj)
+func_to_list(struct workspace *wk, obj rcvr, uint32_t args_node, obj *res)
 {
 	if (!interp_args(wk, args_node, NULL, NULL, NULL)) {
 		return false;
 	}
 
-	*obj = get_obj(wk, rcvr)->dat.custom_target.output;
+	*res = get_obj(wk, rcvr)->dat.custom_target.output;
+	return true;
+}
+
+static bool
+func_full_path(struct workspace *wk, obj rcvr, uint32_t args_node, obj *res)
+{
+	if (!interp_args(wk, args_node, NULL, NULL, NULL)) {
+		return false;
+	}
+
+	obj elem;
+	if (!obj_array_flatten_one(wk, get_obj(wk, rcvr)->dat.custom_target.output, &elem)) {
+		interp_error(wk, args_node, "this custom_target has multiple outputs");
+		return false;
+	}
+
+	assert(get_obj(wk, elem)->type = obj_file);
+	*res = get_obj(wk, elem)->dat.file;
 	return true;
 }
 
 const struct func_impl_name impl_tbl_custom_target[] = {
+	{ "full_path", func_full_path },
 	{ "to_list", func_to_list },
 	{ NULL, NULL },
 };
