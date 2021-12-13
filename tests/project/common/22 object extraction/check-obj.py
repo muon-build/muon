2d410f247be6767a529b1a30c4957b03492a9771
Stone Tickle <lattis@mochiro.moe>
2021-12-13 07:19:30 -0600

    don't require compile_commands.json in test

--- tests/project/common/22 object extraction/check-obj.py
+++ tests/project/common/22 object extraction/check-obj.py
@@ -10,14 +10,16 @@ output = None
 
 # Only the ninja backend produces compile_commands.json
 if sys.argv[1] == 'ninja':
-    with open('compile_commands.json', 'r') as f:
-        cc = json.load(f)
-    output = set((x['output'] for x in cc))
+    if os.path.exists('compile_commands.json'):
+        with open('compile_commands.json', 'r') as f:
+            cc = json.load(f)
+        output = set((x['output'] for x in cc))
 
 for obj in sys.argv[2:]:
     obj = str(PurePath(obj).relative_to(os.getcwd()))
     if not os.path.exists(obj):
         sys.exit(f'File {obj} not found.')
-    if sys.argv[1] == 'ninja' and obj not in output:
-        sys.exit(1)
+    if output and obj not in output:
+        sys.exit(f'File {obj} not in output.')
+
     print('Verified', obj)
