9c0e7bd0f6a5d6fa2143a58c3bb0a9a87cf2c5ad
Stone Tickle <lattis@mochiro.moe>
2022-01-11 06:47:09 -0600

    allow more failures for subproj(required: false)

--- src/lang/eval.c
+++ src/lang/eval.c
@@ -26,42 +26,49 @@ eval_project(struct workspace *wk, const char *subproject_name, const char *cwd,
 	}
 
 	if (!fs_dir_exists(cwd)) {
+		bool wrap_ok = false;
 		char wrap_path[PATH_MAX], base_path[PATH_MAX];
 		snprintf(wrap_path, PATH_MAX, "%s.wrap", cwd);
 
-		if (fs_file_exists(wrap_path)) {
-			if (!path_dirname(base_path, PATH_MAX, cwd)) {
+		if (!fs_file_exists(wrap_path)) {
+			goto wrap_done;
+		}
+
+		if (!path_dirname(base_path, PATH_MAX, cwd)) {
+			return false;
+		}
+
+		struct wrap wrap = { 0 };
+		if (!wrap_handle(wrap_path, base_path, &wrap)) {
+			goto wrap_cleanup;
+		}
+
+		if (wrap.fields[wf_directory]) {
+			if (!path_join(new_cwd, PATH_MAX, base_path, wrap.fields[wf_directory])) {
 				return false;
 			}
 
-			struct wrap wrap = { 0 };
-			if (!wrap_handle(wrap_path, base_path, &wrap)) {
+			if (!path_dirname(base_path, PATH_MAX, build_dir)) {
+				return false;
+			} else if (!path_join(new_build_dir, PATH_MAX, base_path, wrap.fields[wf_directory])) {
 				return false;
 			}
 
-			if (wrap.fields[wf_directory]) {
-				if (!path_join(new_cwd, PATH_MAX, base_path, wrap.fields[wf_directory])) {
-					return false;
-				}
+			cwd = new_cwd;
+			build_dir = new_build_dir;
 
-				if (!path_dirname(base_path, PATH_MAX, build_dir)) {
-					return false;
-				} else if (!path_join(new_build_dir, PATH_MAX, base_path, wrap.fields[wf_directory])) {
-					return false;
-				}
-
-				cwd = new_cwd;
-				build_dir = new_build_dir;
-
-				if (!path_join(src, PATH_MAX, cwd, "meson.build")) {
-					return false;
-				} else if (!path_join(meson_opts, PATH_MAX, cwd, "meson_options.txt")) {
-					return false;
-				}
+			if (!path_join(src, PATH_MAX, cwd, "meson.build")) {
+				return false;
+			} else if (!path_join(meson_opts, PATH_MAX, cwd, "meson_options.txt")) {
+				return false;
 			}
+		}
 
-			wrap_destroy(&wrap);
-		} else {
+		wrap_ok = true;
+wrap_cleanup:
+		wrap_destroy(&wrap);
+wrap_done:
+		if (!wrap_ok) {
 			if (required) {
 				LOG_E("project %s not found", cwd);
 				return false;
