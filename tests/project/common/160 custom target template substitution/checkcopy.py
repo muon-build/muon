c0c08a1c061903fc6cf3d561dcfbc8e050bdf157
Stone Tickle <lattis@mochiro.moe>
2022-02-19 07:31:30 -0600

    update tests

--- tests/project/common/160 custom target template substitution/checkcopy.py
+++ tests/project/common/160 custom target template substitution/checkcopy.py
@@ -3,7 +3,7 @@
 import sys
 import shutil
 
-if "@INPUT1@" in sys.argv[1]:
+if '@INPUT1@' in sys.argv[1]:
     shutil.copyfile(sys.argv[2], sys.argv[3])
 else:
     sys.exit('String @INPUT1@ not found in "{}"'.format(sys.argv[1]))
--- tests/project/common/48 file grabber/grabber.sh
+++ tests/project/common/48 file grabber/grabber.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+# different from meson: run_command() runs from the project root
 for i in "${MESON_SOURCE_ROOT}/${MESON_SUBDIR:-.}/"*.c; do
   echo $i
 done
