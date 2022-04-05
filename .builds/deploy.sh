27caacbd8d89329f43ad7087a9aea8eaa37b5552
Stone Tickle <lattis@mochiro.moe>
2022-04-05 08:02:31 -0500

    deploy website from srht ci

--- .builds/deploy.sh
+++ .builds/deploy.sh
@@ -4,10 +4,17 @@ set -eux
 
 arch=$1
 
-if [ "$(git rev-parse master)" = "$(git rev-parse HEAD)" ]; then
-	sshopts="-o StrictHostKeyChecking=no -p 2975"
-	rsync --rsh="ssh $sshopts" \
-		"muon-$arch" \
-		"muon-$arch.md5" \
-		deploy@mochiro.moe:muon/
+if [ "$(git rev-parse master)" != "$(git rev-parse HEAD)" ]; then
+	exit 0
 fi
+
+sshopts="-o StrictHostKeyChecking=no -p 2975"
+
+rsync --rsh="ssh $sshopts" \
+	"muon-$arch" \
+	"muon-$arch.md5" \
+	deploy@mochiro.moe:muon/releases
+
+rsync --rsh="ssh $sshopts" -r --delete \
+	"doc/website" \
+	deploy@mochiro.moe:muon
