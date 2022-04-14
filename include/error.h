12921b9436722b2a6c5656ee13b7cff953c922bc
Stone Tickle <lattis@mochiro.moe>
2022-04-14 10:22:42 -0500

    add UNREACHABLE and UNREACHABLE_RETURN macro

--- include/error.h
+++ include/error.h
@@ -1,9 +1,13 @@
 #ifndef MUON_ERROR_H
 #define MUON_ERROR_H
 
+#include <assert.h>
 #include <stdarg.h>
 
 #include "platform/filesystem.h"
+
+#define UNREACHABLE assert(false && "unreachable")
+#define UNREACHABLE_RETURN do { assert(false && "unreachable"); return 0; } while (0)
 
 void error_unrecoverable(const char *fmt, ...) __attribute__ ((format(printf, 1, 2)));
 void error_message(struct source *src, uint32_t line, uint32_t col, const char *msg);
