94042774e884b98b528d0926cae02e87609619f5
illiliti <illiliti@dimension.sh>
2022-04-17 10:21:15 +0300

    implement both_libraries.found()

--- src/functions/both_libs.c
+++ src/functions/both_libs.c
@@ -38,9 +38,22 @@ func_both_libs_full_path(struct workspace *wk, obj rcvr, uint32_t args_node, obj
 	return true;
 }
 
+static bool
+func_both_libs_found(struct workspace *wk, obj rcvr, uint32_t args_node, obj *res)
+{
+	if (!interp_args(wk, args_node, NULL, NULL, NULL)) {
+		return false;
+	}
+
+	make_obj(wk, res, obj_bool);
+	set_obj_bool(wk, *res, true);
+	return true;
+}
+
 const struct func_impl_name impl_tbl_both_libs[] = {
 	{ "get_shared_lib", func_both_libs_get_shared_lib },
 	{ "get_static_lib", func_both_libs_get_static_lib },
+	{ "found", func_both_libs_found },
 	{ "full_path", func_both_libs_full_path },
 	{ "path", func_both_libs_full_path },
 	{ NULL, NULL },
