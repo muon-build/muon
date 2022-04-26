ba808b88e8e99cebdef76022f60cbeb80792762f
Stone Tickle <lattis@mochiro.moe>
2022-04-25 20:01:27 -0500

    accept optional argument in find_python()

--- src/functions/modules/python.c
+++ src/functions/modules/python.c
@@ -5,20 +5,26 @@
 #include "platform/filesystem.h"
 
 static bool
-func_module_python_find_python(struct workspace *wk, obj rcvr, uint32_t args_node, obj *obj)
+func_module_python_find_python(struct workspace *wk, obj rcvr, uint32_t args_node, obj *res)
 {
-	if (!interp_args(wk, args_node, NULL, NULL, NULL)) {
+	struct args_norm ao[] = { { obj_string }, ARG_TYPE_NULL };
+	if (!interp_args(wk, args_node, NULL, ao, NULL)) {
 		return false;
 	}
 
+	const char *cmd = "python3";
+	if (ao[0].set) {
+		cmd = get_cstr(wk, ao[0].val);
+	}
+
 	const char *cmd_path;
-	if (!fs_find_cmd("python3", &cmd_path)) {
+	if (!fs_find_cmd(cmd, &cmd_path)) {
 		interp_error(wk, args_node, "python3 not found");
 		return false;
 	}
 
-	make_obj(wk, obj, obj_external_program);
-	struct obj_external_program *ep = get_obj_external_program(wk, *obj);
+	make_obj(wk, res, obj_external_program);
+	struct obj_external_program *ep = get_obj_external_program(wk, *res);
 	ep->found = true;
 	ep->full_path = make_str(wk, cmd_path);
 	return true;
