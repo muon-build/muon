f11b56ecf74daefc2a805242bdaad6e09f352b5b
Stone Tickle <lattis@mochiro.moe>
2022-08-20 14:25:46 -0500

    update the github mirror from ci

---
+++ .builds/push_to_gh_mirror.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -eux
+
+cat >> ~/.ssh/config <<EOF
+Host github.com
+    IdentityFile ~/.ssh/18083346-dfba-4050-bc05-413561f99228
+    IdentitiesOnly yes
+    StrictHostKeyChecking no
+EOF
+
+chmod 600 ~/.ssh/config
+
+git remote add github git@github.com:annacrombie/muon.git
+git push --force --all github
