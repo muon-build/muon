b3303d30d118752b0d920859bc269a30327eb9c9
Stone Tickle <lattis@mochiro.moe>
2022-08-25 15:01:32 -0500

    remove all dependencies from tools/embedder.c
    Remove the need to build the libplatform.a.

--- tools/embedder.c
+++ tools/embedder.c
@@ -1,94 +1,56 @@
 #include "posix.h"
 
-#include "buf_size.h"
-#include "log.h"
-#include "platform/filesystem.h"
-#include "platform/path.h"
+#include <assert.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
 
-static void
-embed_char_array(struct source *src)
+static bool
+embed(const char *path, const char *basename)
 {
+	int c;
 	uint32_t i = 0;
-	fputs("(char []){\n", stdout);
+	FILE *f;
 
-	for (i = 0; i < src->len; ++i) {
-		printf("0x%x, ", src->src[i]);
+	if (!(f = fopen(path, "rb"))) {
+		fprintf(stderr, "couldn't open '%s' for reading\n", path);
+		return false;
+	}
+
+	printf("{ .name = \"%s\", .src = (char []){\n", basename);
+
+	while ((c = fgetc(f)) != EOF) {
+		printf("0x%x, ", c);
 
 		if ((i % 14) == 0) {
 			fputs("\n", stdout);
 		}
+		++i;
 	}
 
-	fputs("0x0\n}", stdout);
-}
+	fputs("0x0\n} },\n", stdout);
 
-static void
-embed_string(struct source *src)
-{
-	uint32_t i = 0;
-	for (i = 0; i < src->len; ++i) {
-		if (i == 0) {
-			putc('"', stdout);
-		}
-
-		if (src->src[i] == '\n') {
-			printf("\\n\"\n");
-			if (i != src->len - 1) {
-				putc('"', stdout);
-			}
-		} else {
-			putc(src->src[i], stdout);
-		}
-	}
-}
-
-static bool
-embed(const char *path)
-{
-	struct source src = { 0 };
-	if (!fs_read_entire_file(path, &src)) {
-		return false;
-	}
-
-	char name[PATH_MAX];
-	if (!path_basename(name, PATH_MAX, path)) {
-		return false;
-	}
-	;
-	printf("{ .name = \"%s\", .src = ", name);
-
-	if (src.len < 4096) {
-		embed_string(&src);
-	} else {
-		embed_char_array(&src);
-	}
-
-	printf("},\n");
-
-	fs_source_destroy(&src);
-	return true;
+	return fclose(f) == 0;
 }
 
 int
 main(int argc, char *const argv[])
 {
-	log_init();
-
 	assert(argc >= 1);
+	assert(((argc - 1) & 1) == 0 && "you must pass an even number of arguments");
 
 	printf("uint32_t embedded_len = %d;\n"
 		"\n"
 		"static struct embedded_file embedded[] = {\n"
 		,
-		argc - 1);
+		(argc - 1) / 2);
 
 	uint32_t i;
-	for (i = 1; i < (uint32_t)argc; ++i) {
-		if (!embed(argv[i])) {
+	for (i = 1; i < (uint32_t)argc; i += 2) {
+		if (!embed(argv[i], argv[i + 1])) {
 			return 1;
 		}
 	}
 
 	printf("};\n");
-	return 0;
 }
