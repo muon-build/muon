6509e1c2f6e4c24e686b6691f4c019547dddc36e
Stone Tickle <lattis@mochiro.moe>
2022-09-23 13:17:05 -0400

    add getopt from musl on windows

---
+++ include/platform/windows/unistd.h
@@ -0,0 +1,6 @@
+#ifndef MUON_PLATFORM_WINDOWS_UNISTD_H
+#define MUON_PLATFORM_WINDOWS_UNISTD_H
+int getopt(int, char * const [], const char *);
+extern char *optarg;
+extern int optind, opterr, optopt;
+#endif
