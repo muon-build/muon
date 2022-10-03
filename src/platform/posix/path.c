b16fa5849cc4a291e38b0755c599312f25e927a6
Vincent Torri <vtorri@outlook.fr>
2022-10-03 05:56:15 +0200

    Windows: fix path_is_basename()

--- src/platform/posix/path.c
+++ src/platform/posix/path.c
@@ -1,9 +1,17 @@
 #include "posix.h"
 
+#include <string.h>
+
 #include "platform/path.h"
 
 bool
 path_is_absolute(const char *path)
 {
 	return *path == PATH_SEP;
+}
+
+bool
+path_is_basename(const char *path)
+{
+	return strchr(path, PATH_SEP) == NULL;
 }
