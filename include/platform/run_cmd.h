1e35f506a7f25792adf059745c45cc05295493ea
Vincent Torri <vtorri@outlook.fr>
2022-10-30 20:02:32 +0100

    fix run_cmd.h

--- include/platform/run_cmd.h
+++ include/platform/run_cmd.h
@@ -8,7 +8,14 @@
 
 #include <stdbool.h>
 #include <stdint.h>
+#ifdef _WIN32
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+#include <windows.h>
+#else
 #include <unistd.h>
+#endif
 
 enum run_cmd_state {
 	run_cmd_error,
@@ -32,16 +39,26 @@ struct run_cmd_ctx {
 	const char *err_msg; // set on error
 	const char *chdir; // set by caller
 	const char *stdin_path; // set by caller
+	int status;
+	enum run_cmd_ctx_flags flags;
+#ifdef _WIN32
+	HANDLE process;
+	struct {
+		OVERLAPPED overlap;
+		HANDLE pipe[2];
+	} pipe_out, pipe_err;
+#else
 	int pipefd_out[2], pipefd_err[2];
 	int input_fd;
-	int status;
 	pid_t pid;
 
 	bool input_fd_open;
 	bool pipefd_out_open[2], pipefd_err_open[2];
-	enum run_cmd_ctx_flags flags;
+#endif
 };
 
+void push_argv_single(const char **argv, uint32_t *len, uint32_t max, const char *arg);
+void argstr_pushall(const char *argstr, uint32_t argc, const char **argv, uint32_t *argi, uint32_t max);
 /*
  * argstr is a NUL delimited array of strings
  * envstr is like argstr, every two strings is considered a key/value pair
