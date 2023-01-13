386c926a8e0f0a83a84d47c390afc2227bb4cb3a
Stone Tickle <lattis@mochiro.moe>
2023-01-13 08:11:43 -0500

    implement external_program.version()

--- src/functions/external_program.c
+++ src/functions/external_program.c
@@ -33,9 +33,21 @@ func_external_program_path(struct workspace *wk, obj rcvr, uint32_t args_node, o
 	return true;
 }
 
+static bool
+func_external_program_version(struct workspace *wk, obj rcvr, uint32_t args_node, obj *res)
+{
+	if (!interp_args(wk, args_node, NULL, NULL, NULL)) {
+		return false;
+	}
+
+	*res = get_obj_external_program(wk, rcvr)->ver;
+	return true;
+}
+
 const struct func_impl_name impl_tbl_external_program[] = {
 	{ "found", func_external_program_found, tc_bool },
 	{ "path", func_external_program_path, tc_string },
 	{ "full_path", func_external_program_path, tc_string },
+	{ "version", func_external_program_version, tc_string },
 	{ NULL, NULL },
 };
