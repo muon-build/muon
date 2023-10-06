ed65e860a3b794b1807819e99a1aa840f86b6be1
Seedo Paul <seedoeldhopaul@gmail.com>
2023-10-06 22:45:00 +0200

    mod/python: implement kwargs `required` and `disabler`
    Signed-off-by: Seedo Paul <seedoeldhopaul@gmail.com>

--- src/functions/modules/python.c
+++ src/functions/modules/python.c
@@ -35,8 +35,53 @@ func_module_python_find_python(struct workspace *wk, obj rcvr, uint32_t args_nod
 	return true;
 }
 
+static bool
+func_module_python_find_installation(struct workspace *wk,
+	obj rcvr, uint32_t args_node, obj *res)
+{
+	struct args_norm ao[] = { { obj_string }, ARG_TYPE_NULL };
+	enum kwargs {
+		kw_required,
+		kw_disabler,
+	};
+	struct args_kw akw[] = {
+		[kw_required] = { "required", obj_bool },
+		[kw_disabler] = { "disabler", obj_bool },
+		0
+	};
+	if (!interp_args(wk, args_node, NULL, ao, akw)) {
+		return false;
+	}
+
+	bool required = !akw[kw_required].set || get_obj_bool(wk, akw[kw_required].val);
+	bool disabler = akw[kw_disabler].set && get_obj_bool(wk, akw[kw_disabler].val);
+
+	const char *cmd = "python3";
+	if (ao[0].set) {
+		cmd = get_cstr(wk, ao[0].val);
+	}
+
+	SBUF(cmd_path);
+	bool found = fs_find_cmd(wk, &cmd_path, cmd);
+	if (required && !found) {
+		interp_error(wk, args_node, "%s not found", cmd);
+		return false;
+	}
+
+	if (!found && disabler) {
+		*res = disabler_id;
+		return true;
+	}
+
+	make_obj(wk, res, obj_external_program);
+	struct obj_external_program *ep = get_obj_external_program(wk, *res);
+	ep->found = found;
+	ep->full_path = make_str(wk, cmd_path.buf);
+	return true;
+}
+
 const struct func_impl_name impl_tbl_module_python[] = {
-	{ "find_installation", func_module_python_find_python, tc_external_program },
+	{ "find_installation", func_module_python_find_installation, tc_external_program },
 	{ NULL, NULL },
 };
 
