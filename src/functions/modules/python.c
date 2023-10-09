180a078df4d3a7cf4fa0a1564e4e9f4216e2863f
Seedo Paul <seedoeldhopaul@gmail.com>
2023-10-09 22:48:26 +0200

    mod/python: impl key `modules` in find_installation
    Signed-off-by: Seedo Paul <seedoeldhopaul@gmail.com>

--- src/functions/modules/python.c
+++ src/functions/modules/python.c
@@ -8,6 +8,7 @@
 #include "functions/modules/python.h"
 #include "lang/interpreter.h"
 #include "platform/filesystem.h"
+#include "platform/run_cmd.h"
 
 static bool
 func_module_python_find_python(struct workspace *wk, obj rcvr, uint32_t args_node, obj *res)
@@ -37,6 +38,51 @@ func_module_python_find_python(struct workspace *wk, obj rcvr, uint32_t args_nod
 }
 
 static bool
+python_module_present(struct workspace *wk, const char *pythonpath, const char *mod)
+{
+	struct run_cmd_ctx cmd_ctx = { 0 };
+
+	SBUF(importstr);
+	sbuf_pushf(wk, &importstr, "import %s", mod);
+
+	char *const *args = (char *const []){
+		(char *)pythonpath,
+		"-c",
+		importstr.buf,
+		0
+	};
+
+	bool present = run_cmd_argv(&cmd_ctx, args, NULL, 0) && cmd_ctx.status == 0;
+
+	run_cmd_ctx_destroy(&cmd_ctx);
+
+	return present;
+}
+
+struct iter_mod_ctx {
+	const char *pythonpath;
+	uint32_t node;
+	bool required;
+};
+
+static enum iteration_result
+iterate_required_module_list(struct workspace *wk, void *ctx, obj val)
+{
+	struct iter_mod_ctx *_ctx = ctx;
+	const char *mod = get_cstr(wk, val);
+
+	if(python_module_present(wk, _ctx->pythonpath, mod)) {
+		return ir_cont;
+	}
+
+	if (_ctx->required) {
+		interp_error(wk, _ctx->node, "python: required module '%s' not found", mod);
+	}
+
+	return ir_err;
+}
+
+static bool
 func_module_python_find_installation(struct workspace *wk,
 	obj rcvr, uint32_t args_node, obj *res)
 {
@@ -44,10 +90,12 @@ func_module_python_find_installation(struct workspace *wk,
 	enum kwargs {
 		kw_required,
 		kw_disabler,
+		kw_modules,
 	};
 	struct args_kw akw[] = {
 		[kw_required] = { "required", obj_bool },
 		[kw_disabler] = { "disabler", obj_bool },
+		[kw_modules] = { "modules", ARG_TYPE_ARRAY_OF | obj_string },
 		0
 	};
 	if (!interp_args(wk, args_node, NULL, ao, akw)) {
@@ -72,6 +120,30 @@ func_module_python_find_installation(struct workspace *wk,
 	if (!found && disabler) {
 		*res = disabler_id;
 		return true;
+	}
+
+	if (akw[kw_modules].set && found) {
+		bool all_present = obj_array_foreach(wk,
+			akw[kw_modules].val,
+			&(struct iter_mod_ctx){
+				.pythonpath = cmd_path.buf,
+				.node = akw[kw_modules].node,
+				.required = required,
+			},
+			iterate_required_module_list
+		);
+
+		if (!all_present) {
+			if (required) {
+				return false;
+			}
+			if (disabler) {
+				*res = disabler_id;
+				return true;
+			}
+			/* Return a not-found object. */
+			found = false;
+		}
 	}
 
 	make_obj(wk, res, obj_external_program);
