bdf96a1c3cf0bcb8488b32e96ec80092438dd7b8
Stone Tickle <lattis@mochiro.moe>
2024-01-24 20:56:14 -0500

    refactor samu log parser to not use f* calls

--- src/external/samurai/log.c
+++ src/external/samurai/log.c
@@ -12,154 +12,192 @@
 #include <string.h>
 
 #include "external/samurai/ctx.h"
+#include "formats/lines.h"
+#include "log.h"
 
 #include "external/samurai/graph.h"
 #include "external/samurai/log.h"
 #include "external/samurai/util.h"
 
 static const char *samu_logname = ".ninja_log";
-static const char *samu_logtmpname = ".ninja_log.tmp";
-static const char *samu_logfmt = "# ninja log v%d\n";
+static const char *samu_log_version_fmt = "# ninja log v%d\n";
 static const int samu_logver = 5;
 
-static char *
-samu_nextfield(char **end)
+enum samu_log_field {
+	samu_log_field_start_time,
+	samu_log_field_end_time,
+	samu_log_field_mtime,
+	samu_log_field_output_path,
+	samu_log_field_command_hash,
+	samu_log_field_count,
+};
+
+struct samu_log_parse_ctx {
+	uint32_t line_no;
+	size_t nentry;
+	struct samu_ctx *samu_ctx;
+};
+
+static enum iteration_result
+samu_log_parse_cb(void *_ctx, char *line, size_t len)
 {
-	char *s = *end;
+	struct samu_log_parse_ctx *ctx = _ctx;
+	char *fields[samu_log_field_count] = { 0 };
+	struct samu_node *n;
 
-	if (!*s) {
-		samu_warn("corrupt build log: missing field");
-		return NULL;
+	if (ctx->line_no == 1) {
+		int ver;
+		if (sscanf(line, samu_log_version_fmt, &ver) < 1 || ver != samu_logver) {
+			return ir_done;
+		}
+
+		goto cont;
 	}
-	*end += strcspn(*end, "\t\n");
-	if (**end)
-		*(*end)++ = '\0';
 
-	return s;
+	{
+		char *p = line;
+		uint32_t i;
+		for (i = 0; i < samu_log_field_count; ++i) {
+			fields[i] = p;
+			if (!(p = strchr(p, '\t'))) {
+				break;
+			}
+
+			*p = 0;
+			++p;
+		}
+	}
+
+	{ // get node
+		if (!fields[samu_log_field_output_path]) {
+			samu_warn("missing output path");
+			goto corrupt_line;
+		}
+
+		n = samu_nodeget(ctx->samu_ctx, fields[samu_log_field_output_path], 0);
+		if (!n || !n->gen) {
+			goto cont;
+		}
+		if (n->logmtime == SAMU_MTIME_MISSING) {
+			++ctx->nentry;
+		}
+	}
+
+	{ // get mtime
+		if (!fields[samu_log_field_mtime]) {
+			samu_warn("missing mtime");
+			goto corrupt_line;
+		}
+
+		char *endptr;
+		n->logmtime = strtoll(fields[samu_log_field_mtime], &endptr, 10);
+		if (*endptr) {
+			samu_warn("invalid mtime: %s", fields[samu_log_field_mtime]);
+			goto corrupt_line;
+		}
+	}
+
+	{ // get output hash
+		if (!fields[samu_log_field_command_hash]) {
+			samu_warn("missing command hash");
+			goto corrupt_line;
+		}
+
+		char *endptr;
+		n->hash = strtoull(fields[samu_log_field_command_hash], &endptr, 16);
+		if (*endptr) {
+			samu_warn("invalid hash for '%s'", n->path->s);
+			goto corrupt_line;
+		}
+	}
+
+cont:
+	++ctx->line_no;
+	return ir_cont;
+corrupt_line:
+	samu_warn("corrupt build log @ line %d", ctx->line_no);
+	goto cont;
+}
+
+static void
+samu_log_open_and_write(struct samu_ctx *ctx, const char *builddir, bool write_graph)
+{
+	const struct samu_edge *e;
+	struct samu_node *n;
+	uint32_t i;
+
+	const char *logpath;
+	if (builddir) {
+		char *path;
+		samu_xasprintf(&ctx->arena, &path, "%s/%s", builddir, samu_logname);
+		logpath = path;
+	} else {
+		logpath = samu_logname;
+	}
+
+	if (!(ctx->log.logfile = fs_fopen(logpath, "wb"))) {
+		samu_fatal("open %s", logpath);
+		return;
+	}
+
+	fprintf(ctx->log.logfile, samu_log_version_fmt, samu_logver);
+
+	if (write_graph) {
+		for (e = ctx->graph.alledges; e; e = e->allnext) {
+			for (i = 0; i < e->nout; ++i) {
+				n = e->out[i];
+				if (!n->hash) {
+					continue;
+				}
+				samu_logrecord(ctx, n);
+			}
+		}
+	}
 }
 
 void
 samu_loginit(struct samu_ctx *ctx, const char *builddir)
 {
-	int ver;
-	char *logpath = (char *)samu_logname, *logtmppath = (char *)samu_logtmpname, *p, *s;
-	size_t nline, nentry, i;
-	struct samu_edge *e;
-	struct samu_node *n;
-	int64_t mtime;
-	struct samu_buffer buf = {0};
-
-	nline = 0;
-	nentry = 0;
+	char *logpath = (char *)samu_logname;
 
 	if (ctx->log.logfile) {
-		fclose(ctx->log.logfile);
-		ctx->log.logfile = NULL;
+		samu_logclose(ctx);
 	}
-	if (builddir)
-		samu_xasprintf(&ctx->arena, &logpath, "%s/%s", builddir, samu_logname);
-	ctx->log.logfile = fopen(logpath, "r+");
-	if (!ctx->log.logfile) {
-		if (errno != ENOENT)
-			samu_fatal("open %s:", logpath);
-		goto rewrite;
-	}
-	setvbuf(ctx->log.logfile, NULL, _IOLBF, 0);
-	if (fscanf(ctx->log.logfile, samu_logfmt, &ver) < 1)
-		goto rewrite;
-	if (ver != samu_logver)
-		goto rewrite;
 
-	for (;;) {
-		if (buf.cap - buf.len < BUFSIZ) {
-			size_t newcap = buf.cap ? buf.cap * 2 : BUFSIZ;
-			buf.data = samu_xreallocarray(&ctx->arena, buf.data, buf.cap, newcap, 1);
-			buf.cap = newcap;
-		}
-		buf.data[buf.cap - 2] = '\0';
-		if (!fgets(buf.data + buf.len, buf.cap - buf.len, ctx->log.logfile))
-			break;
-		if (buf.data[buf.cap - 2] && buf.data[buf.cap - 2] != '\n') {
-			buf.len = buf.cap - 1;
-			continue;
-		}
-		++nline;
-		p = buf.data;
-		buf.len = 0;
-		if (!samu_nextfield(&p))  /* start time */
-			continue;
-		if (!samu_nextfield(&p))  /* end time */
-			continue;
-		s = samu_nextfield(&p);  /* mtime (used for restat) */
-		if (!s)
-			continue;
-		mtime = strtoll(s, &s, 10);
-		if (*s) {
-			samu_warn("corrupt build log: invalid mtime");
-			continue;
-		}
-		s = samu_nextfield(&p);  /* output path */
-		if (!s)
-			continue;
-		n = samu_nodeget(ctx, s, 0);
-		if (!n || !n->gen)
-			continue;
-		if (n->logmtime == SAMU_MTIME_MISSING)
-			++nentry;
-		n->logmtime = mtime;
-		s = samu_nextfield(&p);  /* command hash */
-		if (!s)
-			continue;
-		n->hash = strtoull(s, &s, 16);
-		if (*s) {
-			samu_warn("corrupt build log: invalid hash for '%s'", n->path->s);
-			continue;
-		}
+	if (builddir) {
+		samu_xasprintf(&ctx->arena, &logpath, "%s/%s", builddir, samu_logname);
 	}
-	if (ferror(ctx->log.logfile)) {
-		samu_warn("build log read:");
-		goto rewrite;
-	}
-	if (nline <= 100 || nline <= 3 * nentry) {
+
+	if (!fs_exists(logpath)) {
+		samu_log_open_and_write(ctx, builddir, false);
 		return;
 	}
 
-rewrite:
-	if (ctx->log.logfile) {
-		fclose(ctx->log.logfile);
-		ctx->log.logfile = NULL;
+	struct source src = { 0 };
+	if (!fs_read_entire_file(logpath, &src)) {
+		samu_fatal("failed to read log file at %s", logpath);
 	}
-	if (builddir)
-		samu_xasprintf(&ctx->arena, &logtmppath, "%s/%s", builddir, samu_logtmpname);
-	ctx->log.logfile = fopen(logtmppath, "w");
-	if (!ctx->log.logfile)
-		samu_fatal("open %s:", logtmppath);
-	setvbuf(ctx->log.logfile, NULL, _IOLBF, 0);
-	fprintf(ctx->log.logfile, samu_logfmt, samu_logver);
-	if (nentry > 0) {
-		for (e = ctx->graph.alledges; e; e = e->allnext) {
-			for (i = 0; i < e->nout; ++i) {
-				n = e->out[i];
-				if (!n->hash)
-					continue;
-				samu_logrecord(ctx, n);
-			}
-		}
-	}
-	fflush(ctx->log.logfile);
-	if (ferror(ctx->log.logfile))
-		samu_fatal("build log write failed");
-	if (rename(logtmppath, logpath) < 0)
-		samu_fatal("build log rename:");
+
+	struct samu_log_parse_ctx samu_log_parse_ctx = {
+		.line_no = 1,
+		.samu_ctx = ctx,
+	};
+
+	each_line((char *)src.src, src.len, &samu_log_parse_ctx, samu_log_parse_cb);
+
+	fs_source_destroy(&src);
+
+	/* if (samu_log_parse_ctx.line_no <= 100 || samu_log_parse_ctx.line_no <= 3 * samu_log_parse_ctx.nentry) { */
+	/* 	return; */
+	/* } */
+
+	samu_log_open_and_write(ctx, builddir, true);
 }
 
 void
 samu_logclose(struct samu_ctx *ctx)
 {
-	fflush(ctx->log.logfile);
-	if (ferror(ctx->log.logfile))
-		samu_fatal("build log write failed");
-	fclose(ctx->log.logfile);
+	fs_fclose(ctx->log.logfile);
 	ctx->log.logfile = NULL;
 }
 
