8dd8ed10ab45a1accca5ec90fe21b9774c46a866
Stone Tickle <lattis@mochiro.moe>
2024-07-17 21:25:16 -0400

    try to enable a conformant preprocessor on windows

--- bootstrap.bat
+++ bootstrap.bat
@@ -10,7 +10,7 @@ if "%~1" == "" goto :usage
 set dir=%1
 if not exist "%dir%" mkdir "%dir%"
 
-call cl /nologo /Zi /Iinclude src/amalgam.c /link /out:"%dir%/muon-bootstrap.exe"
+call cl /nologo /Zi /std:c11 /Iinclude src/amalgam.c /link /out:"%dir%/muon-bootstrap.exe"
 goto :eof
 
 :usage
