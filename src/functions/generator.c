99204d99b9b44df05eef4c5ed318e37e66ce9042
Stone Tickle <lattis@mochiro.moe>
2024-08-11 14:42:02 -0400

    generator: support chained generators

--- src/functions/generator.c
+++ src/functions/generator.c
@@ -11,135 +11,126 @@
 #include "functions/generator.h"
 #include "functions/kernel/custom_target.h"
 #include "lang/func_lookup.h"
+#include "lang/object_iterators.h"
 #include "lang/typecheck.h"
 #include "log.h"
 #include "platform/path.h"
 
-struct generated_list_process_for_target_ctx {
-	uint32_t node;
-	struct obj_generator *g;
-	struct obj_generated_list *gl;
-	const char *dir;
-	bool add_targets, generated_include;
-	obj *res;
-
-	// for generated_list_process_for_target_result_iter only
-	obj name;
-	obj custom_target, tmp_arr;
-	struct obj_custom_target *t;
-};
-
-static enum iteration_result
-generated_list_process_for_target_result_iter(struct workspace *wk, void *_ctx, obj file)
+bool
+generated_list_process_file(struct workspace *wk,
+	uint32_t node,
+	struct obj_generator *g,
+	struct obj_generated_list *gl,
+	const char *dir,
+	bool add_targets,
+	obj val,
+	obj *res,
+	bool *generated_include)
 {
-	struct generated_list_process_for_target_ctx *ctx = _ctx;
-
-	obj_array_push(wk, ctx->tmp_arr, file);
-
-	if (ctx->add_targets) {
-		const char *generated_path = get_cstr(wk, *get_obj_file(wk, file));
-
-		enum compiler_language l;
-		if (!ctx->generated_include && filename_to_compiler_language(generated_path, &l)
-			&& languages[l].is_header) {
-			ctx->generated_include = true;
-		}
-
-		SBUF(rel);
-		path_relative_to(wk, &rel, wk->build_root, generated_path);
-
-		str_app(wk, &ctx->name, " ");
-		str_app(wk, &ctx->name, rel.buf);
-	}
-
-	return ir_cont;
-}
-
-static enum iteration_result
-generated_list_process_for_target_iter(struct workspace *wk, void *_ctx, obj val)
-{
-	struct generated_list_process_for_target_ctx *ctx = _ctx;
-
 	SBUF(path);
-	const char *output_dir = ctx->dir;
+	const char *output_dir = dir;
 
-	if (ctx->gl->preserve_path_from) {
-		const char *src = get_file_path(wk, val), *base = get_cstr(wk, ctx->gl->preserve_path_from);
+	if (gl->preserve_path_from) {
+		const char *src = get_file_path(wk, val), *base = get_cstr(wk, gl->preserve_path_from);
 		assert(path_is_subpath(base, src));
 
-		SBUF(dir);
+		SBUF(dest_dir);
 
 		path_relative_to(wk, &path, base, src);
-		path_dirname(wk, &dir, path.buf);
-		path_join(wk, &path, ctx->dir, dir.buf);
+		path_dirname(wk, &dest_dir, path.buf);
+		path_join(wk, &path, dir, dest_dir.buf);
 		output_dir = path.buf;
 	}
 
 	struct make_custom_target_opts opts = {
-		.input_node = ctx->node,
-		.output_node = ctx->node,
-		.command_node = ctx->node,
+		.input_node = node,
+		.output_node = node,
+		.command_node = node,
 		.input_orig = val,
-		.output_orig = ctx->g->output,
+		.output_orig = g->output,
 		.output_dir = output_dir,
-		.build_dir = ctx->dir,
-		.command_orig = ctx->g->raw_command,
-		.depfile_orig = ctx->g->depfile,
-		.capture = ctx->g->capture,
-		.feed = ctx->g->feed,
-		.extra_args = ctx->gl->extra_arguments,
+		.build_dir = dir,
+		.command_orig = g->raw_command,
+		.depfile_orig = g->depfile,
+		.capture = g->capture,
+		.feed = g->feed,
+		.extra_args = gl->extra_arguments,
 		.extra_args_valid = true,
 	};
 
 	obj tgt;
 	if (!make_custom_target(wk, &opts, &tgt)) {
-		return ir_err;
+		return false;
 	}
 
 	struct obj_custom_target *t = get_obj_custom_target(wk, tgt);
-	ctx->custom_target = tgt;
-	ctx->t = t;
 
-	t->env = ctx->gl->env;
+	t->env = gl->env;
 
-	make_obj(wk, &ctx->tmp_arr, obj_array);
-
-	if (ctx->add_targets) {
-		ctx->name = make_str(wk, "");
+	obj name;
+	if (add_targets) {
+		name = make_str(wk, "");
 	}
 
-	if (!obj_array_foreach(wk, t->output, ctx, generated_list_process_for_target_result_iter)) {
-		return ir_err;
-	}
+	{
+		obj tmp_arr, file;
+		make_obj(wk, &tmp_arr, obj_array);
 
-	obj_array_extend_nodup(wk, *ctx->res, ctx->tmp_arr);
+		obj_array_for(wk, t->output, file) {
+			obj_array_push(wk, tmp_arr, file);
 
-	if (ctx->add_targets) {
-		ctx->t->name = make_strf(wk, "<gen:%s>", get_cstr(wk, ctx->name));
-		if (ctx->g->depends) {
-			obj_array_extend(wk, ctx->t->depends, ctx->g->depends);
+			if (add_targets) {
+				const char *generated_path = get_cstr(wk, *get_obj_file(wk, file));
+
+				enum compiler_language l;
+				if (!*generated_include && filename_to_compiler_language(generated_path, &l)
+					&& languages[l].is_header) {
+					*generated_include = true;
+				}
+
+				SBUF(rel);
+				path_relative_to(wk, &rel, wk->build_root, generated_path);
+
+				str_app(wk, &name, " ");
+				str_app(wk, &name, rel.buf);
+			}
 		}
-		obj_array_push(wk, current_project(wk)->targets, ctx->custom_target);
+
+		obj_array_extend_nodup(wk, *res, tmp_arr);
 	}
 
-	return ir_cont;
+	if (add_targets) {
+		t->name = make_strf(wk, "<gen:%s>", get_cstr(wk, name));
+		if (g->depends) {
+			obj_array_extend(wk, t->depends, g->depends);
+		}
+		obj_array_push(wk, current_project(wk)->targets, tgt);
+	}
+
+	return true;
 }
 
 bool
-generated_list_process_for_target(struct workspace *wk, uint32_t err_node, obj gl, obj tgt, bool add_targets, obj *res)
+generated_list_process_for_target(struct workspace *wk,
+	uint32_t node,
+	obj generated_list,
+	obj build_target,
+	bool add_targets,
+	obj *res)
 {
-	struct obj_generated_list *list = get_obj_generated_list(wk, gl);
+	struct obj_generated_list *gl = get_obj_generated_list(wk, generated_list);
+	struct obj_generator *g = get_obj_generator(wk, gl->generator);
 
-	enum obj_type t = get_obj_type(wk, tgt);
+	enum obj_type t = get_obj_type(wk, build_target);
 
-	const char *private_path;
+	const char *dir;
 
 	switch (t) {
-	case obj_both_libs: tgt = get_obj_both_libs(wk, tgt)->dynamic_lib;
+	case obj_both_libs: build_target = get_obj_both_libs(wk, build_target)->dynamic_lib;
 	/* fallthrough */
-	case obj_build_target: private_path = get_cstr(wk, get_obj_build_target(wk, tgt)->private_path); break;
+	case obj_build_target: dir = get_cstr(wk, get_obj_build_target(wk, build_target)->private_path); break;
 	case obj_custom_target: {
-		private_path = get_cstr(wk, get_obj_custom_target(wk, tgt)->private_path);
+		dir = get_cstr(wk, get_obj_custom_target(wk, build_target)->private_path);
 		break;
 	}
 	default: UNREACHABLE;
@@ -147,54 +138,42 @@ generated_list_process_for_target(struct workspace *wk, uint32_t err_node, obj g
 
 	make_obj(wk, res, obj_array);
 
-	struct generated_list_process_for_target_ctx ctx = {
-		.node = err_node,
-		.g = get_obj_generator(wk, list->generator),
-		.gl = list,
-		.dir = private_path,
-		.add_targets = add_targets,
-		.res = res,
-	};
+	bool generated_include = false;
 
-	if (!obj_array_foreach(wk, list->input, &ctx, generated_list_process_for_target_iter)) {
-		return false;
+	obj val;
+	obj_array_for(wk, gl->input, val) {
+		if (get_obj_type(wk, val) == obj_generated_list) {
+			obj sub_res;
+			if (!generated_list_process_for_target(wk, node, val, build_target, add_targets, &sub_res)) {
+				return false;
+			}
+
+			obj file;
+			obj_array_for(wk, sub_res, file) {
+				if (!generated_list_process_file(
+					    wk, node, g, gl, dir, add_targets, file, res, &generated_include)) {
+					return false;
+				}
+			}
+			continue;
+		}
+
+		if (!generated_list_process_file(wk, node, g, gl, dir, add_targets, val, res, &generated_include)) {
+			return false;
+		}
 	}
 
-	if (add_targets && t == obj_build_target && ctx.generated_include) {
-		get_obj_build_target(wk, tgt)->flags |= build_tgt_generated_include;
+	if (add_targets && t == obj_build_target && generated_include) {
+		get_obj_build_target(wk, build_target)->flags |= build_tgt_generated_include;
 	}
 
 	return true;
 }
 
-struct check_preserve_path_from_ctx {
-	const struct obj_generated_list *gl;
-	uint32_t err_node;
-};
-
-static enum iteration_result
-check_preserve_path_from_iter(struct workspace *wk, void *_ctx, obj f)
-{
-	const struct check_preserve_path_from_ctx *ctx = _ctx;
-
-	const char *src = get_file_path(wk, f), *base = get_cstr(wk, ctx->gl->preserve_path_from);
-
-	if (!path_is_subpath(base, src)) {
-		vm_error_at(wk,
-			ctx->err_node,
-			"source file '%s' is not a subdir of preserve_path_from path '%s'",
-			src,
-			base);
-		return ir_err;
-	}
-
-	return ir_cont;
-}
-
 static bool
 func_generator_process(struct workspace *wk, obj gen, obj *res)
 {
-	struct args_norm an[] = { { TYPE_TAG_GLOB | tc_coercible_files }, ARG_TYPE_NULL };
+	struct args_norm an[] = { { TYPE_TAG_GLOB | tc_coercible_files | tc_generated_list }, ARG_TYPE_NULL };
 	enum kwargs {
 		kw_extra_args,
 		kw_preserve_path_from,
@@ -221,8 +200,21 @@ func_generator_process(struct workspace *wk, obj gen, obj *res)
 		return false;
 	}
 
-	if (!coerce_files(wk, an[0].node, an[0].val, &gl->input)) {
-		return false;
+	make_obj(wk, &gl->input, obj_array);
+	{
+		obj v, coercible_files;
+		make_obj(wk, &coercible_files, obj_array);
+
+		obj_array_for(wk, an[0].val, v) {
+			obj_array_push(wk, get_obj_type(wk, v) == obj_generated_list ? gl->input : coercible_files, v);
+		}
+
+		obj files;
+		if (!coerce_files(wk, an[0].node, coercible_files, &files)) {
+			return false;
+		}
+
+		obj_array_extend_nodup(wk, gl->input, files);
 	}
 
 	if (gl->preserve_path_from) {
@@ -231,10 +223,18 @@ func_generator_process(struct workspace *wk, obj gen, obj *res)
 			return false;
 		}
 
-		struct check_preserve_path_from_ctx ctx = { .gl = gl, .err_node = akw[kw_preserve_path_from].node };
+		obj f;
+		obj_array_for(wk, gl->input, f) {
+			const char *src = get_file_path(wk, f), *base = get_cstr(wk, gl->preserve_path_from);
 
-		if (!obj_array_foreach(wk, gl->input, &ctx, check_preserve_path_from_iter)) {
-			return false;
+			if (!path_is_subpath(base, src)) {
+				vm_error_at(wk,
+					akw[kw_preserve_path_from].node,
+					"source file '%s' is not a subdir of preserve_path_from path '%s'",
+					src,
+					base);
+				return false;
+			}
 		}
 	}
 
