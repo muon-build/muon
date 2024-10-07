fee6b49286ae2f93b3ac3aae70f15d5b34b3c7c1
Stone Tickle <lattis@mochiro.moe>
2024-10-07 09:15:19 -0500

    alpine ci: deploy artifacts from non-master branch

--- tools/ci/release.sh
+++ tools/ci/release.sh
@@ -11,12 +11,21 @@ version=""
 # shellcheck disable=SC1091
 . "$build/version.sh"
 
+branch_name="$(git branch --contains HEAD | sed 's/^[* ]*//g' | grep '^[^ ]*$')"
+
+echo "version: $version, branch_name: '$branch_name'"
+
 tools/ci/prepare_binary.sh "$build" "$version-amd64-linux-static"
 tools/ci/prepare_binary.sh "$build_small" "$version-amd64-linux-static-small"
-
 tools/ci/prepare_release_docs.sh "$build"
 tools/ci/prepare_tarball.sh "muon-$version"
-tools/ci/deploy.sh / -r --delete build/doc/website
+
+# only allow master and release branches through
+if [ "$branch_name" = "master" ]; then :
+elif echo "$branch_name" | grep -q '[0-9]\+\.[0-9]\+'; then :
+else exit 0
+fi
+
 tools/ci/deploy.sh "/releases/$version" -r --delete build/doc/docs
 tools/ci/deploy.sh "/releases/$version" \
 	"build/muon-$version-amd64-linux-static" \
@@ -24,3 +33,10 @@ tools/ci/deploy.sh "/releases/$version" \
 	"build-small/muon-$version-amd64-linux-static-small" \
 	"build-small/muon-$version-amd64-linux-static-small.md5" \
 	"muon-$version.tar.gz"
+
+# only allow master through
+if [ "$branch_name" = "master" ]; then :
+else exit 0
+fi
+
+tools/ci/deploy.sh / -r --delete build/doc/website
