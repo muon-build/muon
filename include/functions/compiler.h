b947d775bb735f58e853a999fc848e8b5fee8ea2
Vincent Torri <vtorri@outlook.fr>
2024-12-21 13:55:56 +0100

    Windows: add 'dll.a' as extension when linking agains shared lib. Fixes MSYS2 CI

--- include/functions/compiler.h
+++ include/functions/compiler.h
@@ -13,7 +13,7 @@ enum find_library_flag {
 	find_library_flag_prefer_static = 1 << 0,
 };
 
-#define COMPILER_DYNAMIC_LIB_EXTS ".so", ".dylib", ".dll"
+#define COMPILER_DYNAMIC_LIB_EXTS ".so", ".dylib", ".dll.a", ".dll"
 #define COMPILER_STATIC_LIB_EXTS ".a", ".lib"
 
 obj find_library(struct workspace *wk, const char *name, obj libdirs, enum find_library_flag flags);
