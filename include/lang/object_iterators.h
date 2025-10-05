a77ddd245bfe0167a6366cc68b11d2c2108140fe
Stone Tickle <lattis@mochiro.moe>
2025-10-05 14:22:01 -0400

    object iterators attempt to silence type punning

--- include/lang/object_iterators.h
+++ include/lang/object_iterators.h
@@ -54,7 +54,7 @@ struct obj_dict_for_helper {
 };
 
 #define obj_dict_for_get_kv_big(__iter, __key, __val)                                                \
-	__iter.k = sl_get_((struct slist *)&__iter.h->keys, __iter.i, __iter.h->key_size), __iter.v.u64 = *hash_get(__iter.h, __iter.k), \
+	__iter.k = sl_get_((struct slist *)(char *)&__iter.h->keys, __iter.i, __iter.h->key_size), __iter.v.u64 = *hash_get(__iter.h, __iter.k), \
 	__key = __iter.v.val.key, __val = __iter.v.val.val
 
 #define obj_dict_for_get_kv(__iter, __key, __val) __key = __iter.e->key, __val = __iter.e->val
