029d39e1a8ae1c20ea5c0125cbdb7d4d1c9cdefb
Stone Tickle <lattis@mochiro.moe>
2025-10-26 21:50:45 -0400

    delete unused scripts

--- tools/bootstrap_ninja.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# SPDX-FileCopyrightText: Stone Tickle <lattis@mochiro.moe>
-# SPDX-License-Identifier: GPL-3.0-only
-
-echo "This tool is no longer necessary, muon comes with a bundled samu by default."
