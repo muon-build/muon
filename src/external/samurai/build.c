michaelforney/samurai

build: Propagate mtime of newest input of phony target
If an edge depends on a phony target with inputs, it should be
rebuilt when any of those inputs change.

build.c
@@ -59,11 +59,16 @@ isdirty(struct node *n, struct node *newest, bool generator, bool restat)
 
 	e = n->gen;
 	if (e->rule == &phonyrule) {
-		if (e->nin > 0 || n->mtime != MTIME_MISSING)
-			return false;
-		if (buildopts.explain)
-			warn("explain %s: phony and no inputs", n->path->s);
-		return true;
+		if (n->mtime == MTIME_MISSING) {
+			if (e->nin == 0) {
+				if (buildopts.explain)
+					warn("explain %s: phony and no inputs", n->path->s);
+				return true;
+			}
+			if (newest)
+				n->mtime = newest->mtime;
+		}
+		return false;
 	}
 	if (n->mtime == MTIME_MISSING) {
 		if (buildopts.explain)

muon-build/muon

src/external/samurai/build.c
@@ -53,11 +53,16 @@ samu_isdirty(struct samu_ctx *ctx, struct samu_node *n, struct samu_node *newest
 
 	e = n->gen;
 	if (e->rule == &ctx->phonyrule) {
-		if (e->nin > 0 || n->mtime != SAMU_MTIME_MISSING)
-			return false;
-		if (ctx->buildopts.explain)
-			samu_warn("explain %s: phony and no inputs", n->path->s);
-		return true;
+		if (n->mtime == SAMU_MTIME_MISSING) {
+			if (e->nin == 0) {
+				if (ctx->buildopts.explain)
+					samu_warn("explain %s: phony and no inputs", n->path->s);
+				return true;
+			}
+			if (newest)
+				n->mtime = newest->mtime;
+		}
+		return false;
 	}
 	if (n->mtime == SAMU_MTIME_MISSING) {
 		if (ctx->buildopts.explain)