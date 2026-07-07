eee85542b92221bb0cbfe943d742a3680ad7d51f
Stone Tickle <lattis@mochiro.moe>
2026-07-07 09:29:11 -0400

    muon meson: pass -Ddefault_library=shared

--- src/meson_opts.c
+++ src/meson_opts.c
@@ -472,6 +472,7 @@ translate_meson_opts_setup(struct workspace *wk, char *argv[], uint32_t argc, st
 
 	obj_array_push(wk, ctx->argv, make_str(wk, "setup"));
 
+	obj_array_push(wk, ctx->argv, make_str(wk, "-Ddefault_library=shared"));
 	obj_array_push(wk, ctx->argv, make_str(wk, "-Db_largefile=auto"));
 	obj_array_push(wk, ctx->argv, make_str(wk, "-Db_clang_format=auto"));
 	obj_array_push(wk, ctx->argv, make_str(wk, "-Db_compiler_wrapper=auto"));
