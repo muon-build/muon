24d73edb21baf589e9ccbad2c53272a8377077ef
Stone Tickle <lattis@mochiro.moe>
2026-08-22 20:36:14 -0400

    stub android_exe_type
    Looks like the functionality of this kwarg is just:
    if type == 'executable' && android_exe_type == 'application'
        type = 'dynamic_library'
    endif
    
    Which is pretty suspect since you could just as easily do this at the
    meson level.  I wonder how this got approved?

--- src/functions/kernel/build_target.c
+++ src/functions/kernel/build_target.c
@@ -65,6 +65,7 @@ enum build_target_kwargs {
 	bt_kw_link_args,
 	bt_kw_link_early_args,
 	bt_kw_build_subdir,
+	bt_kw_android_exe_type, // TODO
 
 #define E(lang, s) bt_kw_##lang##s
 #define TOOLCHAIN_ENUM(lang) E(lang, _args), E(lang, _static_args), E(lang, _shared_args), E(lang, _pch),
@@ -1172,6 +1173,7 @@ tgt_common(struct workspace *wk, obj *res, enum tgt_type type, enum tgt_type arg
 		[bt_kw_link_args] = { "link_args", TYPE_TAG_LISTIFY | obj_string },
 		[bt_kw_link_early_args] = { "link_early_args", TYPE_TAG_LISTIFY | obj_string },
 		[bt_kw_build_subdir] = { "build_subdir", obj_string },
+		[bt_kw_android_exe_type] = { "android_exe_type", obj_string },
 #define E(lang, s, t) [bt_kw_##lang##s] = { #lang #s, t }
 #define TOOLCHAIN_ENUM(lang)                                                                                 \
 	E(lang, _args, TYPE_TAG_LISTIFY | obj_string), E(lang, _static_args, TYPE_TAG_LISTIFY | obj_string), \
